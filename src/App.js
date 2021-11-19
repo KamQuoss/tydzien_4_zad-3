ea7047eb1e16fc4654aae5f64da60fba31644f1f
KamQuoss <kamilaklosek@gmail.com>
2021-11-19 16:21:58 +0100

    Reackt Hook Form with MUI added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./styles.css";
+import { Typography, Container } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Form from "./Form";
+
+const theme = createTheme();
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Zadanie 3</h1>
-      <h2>Formularz z React Hook Form</h2>
-      <Form />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="md">
+        <Typography component="h1" variant="h3">
+          Zadanie 3
+        </Typography>
+        <Typography component="h2" variant="h5">
+          Formularz z React Hook Form i MUI
+        </Typography>
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,22 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  TextField,
+  FormHelperText,
+  Checkbox
+} from "@mui/material";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 export default function Form() {
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors }
   } = useForm();
@@ -11,31 +24,174 @@ export default function Form() {
   console.log(errors);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="First name"
-        {...register("First name", { required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <textarea {...register("Bio", { required: true, max: 3000, min: 50 })} />
-
-      <input
-        {...register("Gender", { required: true })}
-        type="radio"
-        value="kobieta, mężczyzna"
-      />
-      <input
-        type="checkbox"
-        placeholder="Acceptance"
-        {...register("Acceptance", { required: true })}
-      />
-
-      <input type="submit" />
-    </form>
+    <Box
+      component="form"
+      sx={{
+        bgcolor: "background.paper",
+        boxShadow: 1,
+        borderRadius: 1,
+        p: 3,
+        mt: 3
+      }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name="Name"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: "Pole nie może być puste",
+              maxLength: {
+                value: 80,
+                message: "Sprawdż poprawność danych"
+              }
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                fullWidth
+                label="Imię i nazwisko"
+                error={errors.Name ? true : false}
+                helperText={errors.Name?.message}
+              />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name="Email"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: "Pole nie może być puste",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Nieprawidłowy adres email"
+              }
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                fullWidth
+                label="email"
+                error={errors.Email ? true : false}
+                helperText={errors.Email?.message}
+              />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Controller
+            name="Bio"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: "Pole nie może być puste",
+              maxLength: {
+                value: 300,
+                message: "Skróć o kilka słów"
+              },
+              minLength: {
+                value: 30,
+                message: "Dopisz jeszcze kilka słów"
+              }
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                required
+                multiline
+                fullWidth
+                rows={3}
+                label="O mnie"
+                placeholder="Napisz coś o sobie"
+                error={errors.Bio ? true : false}
+                helperText={errors.Bio?.message}
+              />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Controller
+            name="Gender"
+            control={control}
+            defaultValue="female"
+            rules={{ required: "Wybierz odpowiedż" }}
+            render={({ field }) => (
+              <FormControl
+                component="fieldset"
+                error={errors.Gender ? true : false}
+              >
+                <FormLabel component="legend">Jestem *</FormLabel>
+                <RadioGroup row aria-label="gender" required {...field}>
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="kobietą"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="mężczyzną"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="osobą nienormatywną"
+                  />
+                </RadioGroup>
+                {errors.Gender && (
+                  <FormHelperText>{errors.Gender?.message}</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Controller
+            name="Acceptance"
+            control={control}
+            defaultValue={false}
+            rules={{
+              required: "Musisz zaakceptować ragulamin, żeby się zapisać"
+            }}
+            render={({
+              field: { onChange, onBlur, value, name, ref },
+              fieldState: { invalid, isTouched, isDirty, error },
+              formState
+            }) => (
+              <FormControl error={error}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      onBlur={onBlur} // notify when input is touched
+                      onChange={onChange} // send value to hook form
+                      checked={value}
+                      inputRef={ref}
+                    />
+                  }
+                  label="Akceptuję regulamin"
+                />
+                {errors.Acceptance && (
+                  <FormHelperText>{errors.Acceptance?.message}</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button variant="contained" type="submit" children="Prześlij" />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
